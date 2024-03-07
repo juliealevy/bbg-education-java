f3e3fabdc9e2272d5fe4f237e649a18719fdb7d2
Julie Levy <jalevy19@gmail.com>
2024-03-07 10:51:32 -0500

    removed some lombok from sessioncourseentity

--- src/main/java/com/play/java/bbgeducation/domain/sessioncourse/SessionCourseEntity.java
+++ src/main/java/com/play/java/bbgeducation/domain/sessioncourse/SessionCourseEntity.java
@@ -3,22 +3,17 @@ package com.play.java.bbgeducation.domain.sessioncourse;
 import com.play.java.bbgeducation.domain.courses.CourseEntity;
 import com.play.java.bbgeducation.domain.programs.SessionEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.time.OffsetDateTime;
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter
+@Setter
 @Entity
 @Table(name="session_course", uniqueConstraints = {@UniqueConstraint(name = "session_id_course_id_unique", columnNames = {"session_id", "course_id"})})
 public class SessionCourseEntity implements Serializable {
@@ -38,7 +33,7 @@ public class SessionCourseEntity implements Serializable {
     @OneToMany(mappedBy = "sessionCourse",fetch = FetchType.LAZY)
     private Set<ClassEntity> classes;
 
-    //want to set as not insertable or updateable, but then findById doesn't return the dates...
+    //want to set as not insertable or updatable, but then findById doesn't return the dates...
     @Column(name = "created_date_time", columnDefinition = "TIMESTAMP WITH TIME ZONE")
     @CreationTimestamp
     private OffsetDateTime createdDateTime;
@@ -47,4 +42,47 @@ public class SessionCourseEntity implements Serializable {
     @UpdateTimestamp
     private OffsetDateTime updatedDateTime;
 
+    public SessionCourseEntity(){
+
+    }
+
+    public static SessionCourseEntity create(SessionEntity session, CourseEntity course){
+        return build(null, session, course);
+    }
+
+    public static SessionCourseEntity build(Long id, SessionEntity session, CourseEntity course){
+        SessionCourseEntity courseEntity = new SessionCourseEntity();
+        courseEntity.id = id;
+        courseEntity.session = session;
+        courseEntity.course = course;
+
+        return courseEntity;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        if (id == null){
+            return false;
+        }
+        SessionCourseEntity that = (SessionCourseEntity) obj;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(14);
+    }
+
+    @Override
+    public String toString() {
+        return "SessionCourseEntity{" +
+                "id=" + id +
+                '}';
+    }
 }
